53e29e43ebecb44cd2705f9568efa93ebebf21a7
BaZz <bazuaye50@gmail.com>
2023-09-04 09:14:52 +0100

    assets: font family inter

--- js/index.js
+++ js/index.js
@@ -4,11 +4,7 @@ import { conns, getDOM_elements } from "./utilities.js";
  * @description : File run codes when dom content iis fully loaded
  * @returns : void
  */
-document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('DOM fully loaded and parsed');
-  conns();
-
-})
-
-
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("DOM fully loaded and parsed");
+	conns();
+});
