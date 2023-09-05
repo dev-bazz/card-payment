fa37454648defd009b7274a68d98031a4e1352cb
BaZz <bazuaye50@gmail.com>
2023-09-05 09:57:51 +0100

    first name and last validation

--- js/index.js
+++ js/index.js
@@ -1,39 +1,35 @@
-import { conns, getDOM_elements } from "./utilities.js";
+import { getDOM_elements } from "./utilities.js";
 
 /**
  * @description : File run codes when dom content iis fully loaded
  * @returns : void
  */
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("DOM fully loaded and parsed");
-
 	cardNumbers();
 });
 
 function cardNumbers() {
 	const regex = new RegExp(/^\d+$/);
-	const cardNumber = getDOM_elements("all", ".card_num");
-	const cardUiNumber = getDOM_elements("all", ".num_ui");
+	const cardNumber = getDOM_elements("all", ".card_num"),
+		cardUiNumber = getDOM_elements("all", ".num_ui"),
+		holderNamer = getDOM_elements("one", ".holderName"),
+		holderUI = getDOM_elements("one", ".holderUI"); //?
 
 	cardNumber.forEach((fourDigit, index) => {
 		fourDigit.addEventListener("focusout", (e) => {
 			const current = e.target;
 			let currentValue = current.value;
 
-			console.log("current value", cardUiNumber[index]);
-			if (!currentValue) {
-				cardUiNumber[index].textContent = "000";
+			if (!currentValue || currentValue.length < 4) {
+				cardUiNumber[index].textContent = "0000";
+				e.target.value = "";
 				return;
 			}
-
 			cardUiNumber[index].textContent = currentValue;
-
-			console.log("card UI", e.target.value);
 		});
 	});
 
 	cardNumber.forEach((fourDigit, index) => {
-		// console.log("digit inputs", fourDigit);
 		fourDigit.addEventListener("input", (e) => {
 			const current = e.target;
 			let currentValue = current.value;
@@ -43,15 +39,28 @@ function cardNumbers() {
 				e.target.value = currentValue.slice(0, -1);
 				return;
 			}
-
 			if (index == cardNumber.length - 1 && currentValue.length == maxDigit) {
 				getDOM_elements("one", ".holderName").focus();
 				return;
 			}
 			if (index < cardNumber.length - 1 && currentValue.length == maxDigit) {
-				console.log("next");
 				cardNumber[index + 1].focus();
 			}
 		});
+	});
+
+	// Holder Name
+	holderNamer.addEventListener("focusout", (e) => {
+		const current = e.target;
+
+		const firstANDlastNames = current.value.split(" ");
+
+		if (firstANDlastNames.length < 2) {
+			current.value = "";
+			holderUI.textContent = "please enter first & last name";
+			return;
+		}
+		holderUI.textContent = `${firstANDlastNames[0]} ${firstANDlastNames[1]}`;
+		console.log(firstANDlastNames);
 	});
 }
