21ab4c4f315380ae9f3653ba732e257653b23edc
BaZz <bazuaye50@gmail.com>
2023-09-05 07:09:07 +0100

    logic card number

--- js/index.js
+++ js/index.js
@@ -6,14 +6,36 @@ import { conns, getDOM_elements } from "./utilities.js";
  */
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("DOM fully loaded and parsed");
-	const gg = getDOM_elements("one", ".card");
 
 	cardNumbers();
 });
 
 function cardNumbers() {
+	const regex = new RegExp(/^\d+$/);
 	const cardNumber = getDOM_elements("all", ".card_num");
-	cardNumber.forEach((card) => {
-		console.log(card);
+	cardNumber.forEach((fourDigit) => {
+		fourDigit.addEventListener("focusout", (e) => {
+			const current = e.target,
+				currentValue = current.value;
+
+			console.log("card UI", e.target.value);
+		});
+	});
+
+	cardNumber.forEach((fourDigit, index) => {
+		fourDigit.addEventListener("input", (e) => {
+			const current = e.target;
+			let currentValue = current.value;
+			const maxDigit = current.getAttribute("maxlength");
+
+			if (!regex.test(currentValue)) {
+				e.target.value = currentValue.slice(0, -1);
+				return;
+			}
+
+			if (index < cardNumber.length - 1 && currentValue.length === maxDigit) {
+				cardNumber[index + 1].focus();
+			}
+		});
 	});
 }
